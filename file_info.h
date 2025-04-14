7ffcae786d40d4df9951a354f18a52f3a9fa14fb
Luis Saenz <145069480+Luisss003@users.noreply.github.com>
2025-04-14 01:39:38 -0500

    Update file_info.h

--- file_info.h
+++ file_info.h
@@ -17,20 +17,4 @@ struct my_pkthdr {
   int len;
 };
 
-struct config_data {
-  ip_addr_t victim_ip;
-  eth_addr_t victim_mac;
-  uint16_t victim_port;
-  ip_addr_t attacker_ip;
-  eth_addr_t attacker_mac;
-  uint16_t attacker_port;
-  ip_addr_t replay_victim_ip;
-  eth_addr_t replay_victim_mac;
-  uint16_t replay_victim_port;
-  ip_addr_t replay_attacker_ip;
-  eth_addr_t replay_attacker_mac;
-  uint16_t replay_attacker_port;
-  char *interface;
-  char *timing;
-}
 #endif
